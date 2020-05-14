a8a44ad7d827d9fd9aec5dff1530e056500b9401
Chinmay <chinmaysharma45@gmail.com>
2020-05-14 17:25:13 +0530

    Updates

---
+++ precache-manifest.5bb4f4dc735d331626b4fac30d47015b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e0bcbc1b4dfff53a46b14f9450e5cef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5554a64bea340890c7ab",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e45a6afe68039955ee4b",
+    "url": "/static/js/2.b3aacced.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b3aacced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5554a64bea340890c7ab",
+    "url": "/static/js/main.a311cd6c.chunk.js"
+  },
+  {
+    "revision": "3360d720e93683c1a4b8",
+    "url": "/static/js/runtime-main.8045aa38.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

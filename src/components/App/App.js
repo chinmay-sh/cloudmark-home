1b8040d088969c1240482f7e160470011f4f7c22
Chinmay <chinmaysharma45@gmail.com>
2020-05-14 17:40:08 +0530

    fix: logo path

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../../logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Mouse
+        <img src={logo} />
       </header>
     </div>
   );

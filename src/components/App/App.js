05ae43988274dd7711bb0dca92ce5eee99aa76e4
Chinmay <chinmaysharma45@gmail.com>
2020-06-02 19:11:08 +0530

    feat: initial version of home

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {Container,Row, Col} from 'react-bootstrap';
 import AppCard from '../AppCard/AppCard';
+import converterImg from './assets/converter.svg';
+import bmiImg from './assets/bmi.png';
+import cryptoImg from './assets/crypto.svg';
+import './App.css';
 
 export default function App() {
     return(
         <Container fluid>
             <Row>
                 <Col>
-                    <h2>Welcome to CloudMark Home</h2>
+                    <h2 className="heading">Welcome to CloudMark Home</h2>
                 </Col>
             </Row>
             <br />
@@ -15,17 +19,17 @@ export default function App() {
 
           <Col>
             <br />
-            <AppCard title="Universal Converter" href="https://the-redlord.github.io/converter/#/" />
+            <AppCard title="Universal Converter" href="https://the-redlord.github.io/converter/#/" img={converterImg} />
           </Col>
 
           <Col>
           <br/>
-            <AppCard title="BMI Calculator" href="https://the-redlord.github.io/bmi_calc_react/" />
+            <AppCard title="BMI Calculator" href="https://the-redlord.github.io/bmi_calc_react/" img={bmiImg} />
           </Col>
 
           <Col>
           <br/>
-            <AppCard title="Crypto Ticker" href="https://the-redlord.github.io/crypto_ticker/" />
+            <AppCard title="Crypto Ticker" href="https://the-redlord.github.io/crypto_ticker/" img={cryptoImg} />
           </Col>
         </Row>
         <br/>
--- src/components/AppCard/AppCard.js
+++ src/components/AppCard/AppCard.js
@@ -4,11 +4,11 @@ import {Card,Button} from 'react-bootstrap';
 export default function AppCard(props){
 
     return(
-        <Card bg="dark" style={{ width: '18rem',borderRadius:"20px",margin:"auto" }}>
-                <Card.Img variant="top" src={weightImg} style={{width:"70%",margin:"auto"}} />
+        <Card bg="dark" style={{ width: '18rem',borderRadius:"20px",margin:"auto" }} text="warning">
+                <Card.Img variant="top" src={props.img} style={{width:"70%",margin:"auto"}} />
                 <Card.Body>
                     <Card.Title>{props.title}</Card.Title>  
-                    <Button variant="info" href={props.href} activeClassName="active" to="/mass">RUN</Button>
+                    <Button variant="success" href={props.href} activeClassName="active">RUN</Button>
                 </Card.Body>
         </Card>
     );

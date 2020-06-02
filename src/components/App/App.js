3493a2d24aafdb5cfc7e37e0af6152d42d390772
Chinmay <chinmaysharma45@gmail.com>
2020-06-02 19:39:29 +0530

    fix: grid

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,17 +17,17 @@ export default function App() {
             <br />
         <Row>
 
-          <Col>
+          <Col xs="12" sm="12" md="6" lg="4" >
             <br />
             <AppCard title="Universal Converter" href="https://the-redlord.github.io/converter/#/" img={converterImg} />
           </Col>
 
-          <Col>
+          <Col xs="12" sm="12" md="6" lg="4">
           <br/>
             <AppCard title="BMI Calculator" href="https://the-redlord.github.io/bmi_calc_react/" img={bmiImg} />
           </Col>
 
-          <Col>
+          <Col xs="12" sm="12" md="6" lg="4">
           <br/>
             <AppCard title="Crypto Ticker" href="https://the-redlord.github.io/crypto_ticker/" img={cryptoImg} />
           </Col>
